6a0d826fe72b8a0df07304f461ab5e2a8b1d5ff5
romeorubiko <raulescabia255@gmail.com>
2018-04-18 17:00:20 +0200

    añadido contador de simbolos, lineas y caracteres

--- Practica_2_p2/src/lex/manual/Driver.java
+++ Practica_2_p2/src/lex/manual/Driver.java
@@ -1,15 +1,18 @@
 package lex.manual;
 
-import lex.generado.*; 
+import lex.generado.*;
+
+import java.io.BufferedReader;
 import java.io.FileInputStream; 
 import java.io.IOException; 
-import java.io.InputStream; 
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.ArrayList; 
 
 public class Driver {
 	public static void main(String args[]) throws IOException{
 		// Entrada de datos: por fichero 
-		InputStream dataStream = new FileInputStream("input.txt");
+		InputStream dataStream = new FileInputStream("grupo_50_prueba1OK.txt");
 		
 		  // Creamos el objeto scanner    
 		 Lexer scanner = new Lexer(dataStream);   
@@ -18,6 +21,7 @@ public class Driver {
 		 // Mientras no alcancemos el fin de la entrada    
 		 boolean end = false;
 		 Symbol token;
+		 int simbolos = 0;
 		 while (!end) {    
 			 try {  
 				 //leemos siguiente token
@@ -25,7 +29,8 @@ public class Driver {
 				 symbols.add(token);     
 				 end = (token.value() == null);     
 				 if (!end) {                                         
-					 System.out.println("Encontrado: {" + token.type() + " - "+SimbolosTerminales.terminalNames[token.type()]+ "} >> " + token.value());       
+					 System.out.println("Encontrado: {" + token.type() + " - "+SimbolosTerminales.terminalNames[token.type()]+ "} >> " + token.value());
+					 simbolos ++;
 					 }    
 				 } 
 			 catch (Exception x) {     
@@ -33,7 +38,22 @@ public class Driver {
 				 x.printStackTrace();    }   } 
 		 
 		 symbols.trimToSize();  
-		 System.out.println("\n\n -- Bye-bye -- "); 
+		 System.out.println("___________________________________________");
+		 //todo: incluir el código adicional para mostrar por pantalla el número de líneas, caracteres y símbolos encontrados en el programa.
+		 BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream("grupo_50_prueba1OK.txt")));
+		 int lineas = 0;
+		 int caracteres = 0;
+		 String lectura = br.readLine();
+		 while (lectura!=null) {
+			 lineas++;
+			 caracteres = caracteres + lectura.length();
+			 lectura = br.readLine();
+		 }
+		 System.out.println("Líneas: "+lineas );
+		 System.out.println("Símbolos: "+simbolos );
+		 System.out.println("Caracteres: "+caracteres );
+		 
+		 System.out.println("\n\n ---------------FIN--------------- "); 
 		 
 	}
 }
